e924af6720655fcc7643bbd88111b53cebd34396
shuto-416 <hsjudo416@gmail.com>
2021-10-12 13:48:43 +0900

    little bit edit header & searching gradiet

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -1,24 +1,16 @@
+import React from 'react'
 import type { NextPage } from 'next'
 import styles from "../styles/Header.module.scss"
 import Link from "next/link"
-import ReactMarkdown from 'react-markdown';
 
 const Header :NextPage = () => {
-
-    const title1 = "[**hama**](/)"; // [title](url) aタグ扱い
-    const title2 = "[**sh**](/)";
-    const title3 = " [**(hama-**](/)";
-    const title4 = "[**shell**](/)";
-    const title5 = "**)**"
 
     return(
         <div className={styles.header_container}>
             <div className={styles.header_title}>
-                <ReactMarkdown children={title1} />
-                <ReactMarkdown className={styles.title} children={title2}/>
-                <ReactMarkdown children={title3}/>
-                <ReactMarkdown className={styles.title} children={title4}/>
-                <ReactMarkdown children={title5}/>
+				<Link href="/">
+					hamash(hama-shell)
+				</Link>
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
+
+  const self_intro = "";
   return (
     <div>
       <Head>
