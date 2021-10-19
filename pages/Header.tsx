273cb4c62454c02279c5a474ab7c609645285f25
shuto-416 <hsjudo416@gmail.com>
2021-10-19 10:06:12 +0900

    edited header & its css with markdown

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import type { NextPage } from 'next'
 import styles from "../styles/Header.module.scss"
-import Link from "next/link"
+import ReactMarkdown from 'react-markdown'
 
 const Header :NextPage = () => {
 
+    const title = "[hamash[hama-shell]](/)"
     return(
         <div className={styles.header_container}>
             <div className={styles.header_title}>
-				<Link href="/">
-					hamash(hama-shell)
-				</Link>
+                <ReactMarkdown children={title} />
             </div>
         </div>
     )
