14890e67fc9e5767d7fe11980313314c5a7fcc9e
shuto-416 <hsjudo416@gmail.com>
2021-10-20 11:07:28 +0900

    cannot remove error about fs & syntax error

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,36 +4,41 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 import CodeBlock from './CodeBlock'
 import { useEffect, useState } from 'react'
+import fs from 'fs'
 
-const readMD = (setMarkdown: Function, setIsLoading: Function): void => {
-  const postName = "body"
-  import(`./${postName}.md`)
-      .then(res => {
-          fetch(res.default)
-              .then(res => res.text())
-              .then(res => {
-                  setMarkdown(res)
-                  setIsLoading(false)
-              })
-      })
-}
+// const readMD = (setMarkdown: Function, setIsLoading: Function): void => {
+//   const postName = "body"
+//   import(`./${postName}.md`)
+//       .then(res => {
+//           fetch(res.default)
+//               .then(res => res.text())
+//               .then(res => {
+//                   setMarkdown(res)
+//                   setIsLoading(false)
+//               })
+//       })
+// }
 
-const Home: NextPage = () => {
+type InitialProps = {
+  markdown: string;
+};
 
-  const [markdown, setMarkdown] = useState<string>('')
-    const [isLoading, setIsLoading] = useState<boolean>(true)
+const Home: NextPage<InitialProps> = props => {
 
-    useEffect(() => {
-      setTimeout(() => { 
-          readMD(setMarkdown, setIsLoading)
-      }, 2000);
-  }, [])
+  // const [markdown, setMarkdown] = useState<string>('')
+  //   const [isLoading, setIsLoading] = useState<boolean>(true)
 
-  if (isLoading) {
-    return (
-        <h1>Hello</h1>
-    )
-}
+  //   useEffect(() => {
+  //     setTimeout(() => { 
+  //         readMD(setMarkdown, setIsLoading)
+  //     }, 2000);
+  // }, [])
+
+  // if (isLoading) {
+  //   return (
+  //       <h1>Hello</h1>
+  //   )
+  // }
   
   return (
     <div>
@@ -42,10 +47,15 @@ const Home: NextPage = () => {
         <meta name="hamash" content="portfolio of ore" />
       </Head>
       <div className={styles.title}>
-        <ReactMarkdown children={markdown} components={{code: CodeBlock}}/>
+        <ReactMarkdown children={props.markdown} components={{code: CodeBlock}}/>
       </div>
     </div>
   )
 }
 
 export default Home
+
+Home.getInitialProps = async () => {
+  const md = fs.readFileSync("./body.md")
+  return {markdown: md.toString()}
+}
